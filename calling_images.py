80ec8fdf51e166211184d449b8c4a9da949b217a
hyorakc-mac <hyorakc@umich.edu>
2019-04-29 21:33:56 -0400

    working

--- calling_images.py
+++ calling_images.py
@@ -1,6 +1,7 @@
 import requests
 import io
 import os
+import sqlite3
 
 # Imports the Google Cloud client library
 from google.cloud import vision
@@ -9,38 +10,47 @@ from google.cloud.vision import types
 # Instantiates a client
 client = vision.ImageAnnotatorClient()
 
+conn = sqlite3.connect('reddit_r_awww.sqlite')
+cur = conn.cursor()
+cur.execute('SELECT thumburl FROM Reddit')
+post_list = cur.fetchall()
 
-url = "https://b.thumbs.redditmedia.com/P8WJSc0UT53zWMRshZbi7dbyn4dX7xlh4CbR8g-ziQM.jpg"
-response = requests.get(url)
-if response.status_code == 200:
-    # with open("/Users/apple/Desktop/sample.jpg", 'wb') as f:
-    #     f.write(response.content)
-    content = response.content
+for _ in post_list:
 
-
-# Loads the image into memory
-# with io.open(file_name, 'rb') as image_file:
-#     content = image_file.read()
-
-image = types.Image(content=content)
-
-# Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
-
-# print('Labels:')
-# for label in labels:
-#     print(label.description)
+    url = _
+    response = requests.get(url)
+    if response.status_code == 200:
+        # with open("/Users/apple/Desktop/sample.jpg", 'wb') as f:
+        #     f.write(response.content)
+        content = response.content
 
 
-catsvdogs = []
-if 'Cat' in labels:
+    # Loads the image into memory
+    # with io.open(file_name, 'rb') as image_file:
+    #     content = image_file.read()
+
+    image = types.Image(content=content)
+
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    # print('Labels:')
+    # for label in labels:
+    #     print(label.description)
+
+    labels2 = [x.description for x in labels]
+
+
+    catsvdogs = []
+    if 'Cat' in labels2:
     
-    if 'Dog' in labels:
-        catsvdogs.append('Cat and Dog')
-    else:
-        catsvdogs.append("Cat")
-elif 'Dog' in labels:
-    catsvdogs.append('Dog')
+        if 'Dog' in labels2:
+            catsvdogs.append('Cat and Dog')
+        else:
+            catsvdogs.append("Cat")
+    elif 'Dog' in labels2:
+        catsvdogs.append('Dog')
 
-return catsvdogs
+    return catsvdogs
+    # print(catsvdogs)
